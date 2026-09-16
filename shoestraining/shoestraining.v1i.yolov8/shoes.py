83112ab016ea109c7b0ea8544e5fe8f5c295de23
kingfear42 <abdullahkerem.korkmaz0@gmail.com>
2026-09-16 11:01:28 +0300

    Updated

--- shoestraining/shoestraining.v1i.yolov8/shoes.py
+++ shoestraining/shoestraining.v1i.yolov8/shoes.py
@@ -5,9 +5,9 @@ base_dir = r"C:\Users\krkmz\Desktop\Aware Robotics Opencv\shoestraining\shoestra
 
 model_path = os.path.join(base_dir, r"C:\Users\krkmz\Desktop\Aware Robotics Opencv\shoestraining\shoestraining.v1i.yolov8\runs\segment\yolov8_custom_model\weights\best.pt") 
 
-source_path = os.path.join(base_dir, r"C:\Users\krkmz\Desktop\Aware Robotics Opencv\shoestraining\shoestraining.v1i.yolov8\test\images\taban_33_png.rf.84dc2e1ee8f2eac296a399ec5d4710bb.jpg") 
+source_path = os.path.join(base_dir, r"C:\Users\krkmz\Desktop\Aware Robotics Opencv\shoestraining\shoestraining.v1i.yolov8\test\images\taban2.jpg") 
 
 print("Fotoğraf işleniyor ve ekranda gösteriliyor...")
 
 model = YOLO(model_path)
-results = model.predict(source=source_path, conf=0.3, show=True, save=True)
+results = model.predict(source=source_path, conf=0.05, show=True, save=True)
